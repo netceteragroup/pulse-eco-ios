4a2cf4da61c42930f586eab027fec6f9be03d383
Monika Dimitrova <Monika.Dimitrova@netcetera.com>
2020-06-18 15:14:32 +0200

    add comment for all sensor statuses and types

--- shared/Models/SensorStatus.swift
+++ shared/Models/SensorStatus.swift
@@ -1,16 +1,21 @@
 import Foundation
 
 enum SensorStatus: String, Codable {
+    // The sensor is up and running properly
     case active = "ACTIVE"
+    // The sensor is registered but turned off and ignored
     case inactive = "INACTIVE"
+    // The sensor is registered, but so far not bound to an owner
     case notClaimed = "NOT_CLAIMED"
+    // The sensor is manually removed from evidence in order to keep data sanity
     case banned = "BANNED"
-    //A user requested this location with a device ID, but not sending data yet
+    // A user requested this location with a device ID, but not sending data yet
     case requested = "REQUESTED"
-    //The sensor is up and running properly but not yet confirmed by the community lead
+    // The sensor is up and running properly but not yet confirmed by the community lead
     case activeUnconfirmed = "ACTIVE_UNCONFIRMED"
-    //The sensor is registered, but so far not bound to an owner nor confirmed by the community lead
+    // The sensor is registered, but so far not bound to an owner nor confirmed by the community lead
     case notClaimedUnconfirmed = "NOT_CLAIMED_UNCONFIRMED"
+    // Used as a default value
     case unknown = "UNKNOWN"
     
 }
--- shared/Models/SensorTypes.swift
+++ shared/Models/SensorTypes.swift
@@ -3,25 +3,29 @@ import CoreLocation
 import UIKit
 
 enum SensorType: String, Codable {
+    // MOEPP measurement station
     case MOEPP = "0"
+    // SkopjePulse LoRaWAN based sensor, version 1
     case LoRaWAN = "1"
+    // SkopjePulse WiFi based sensor, version 1
     case Wifi = "2"
+    // Pulse.eco WiFi based sensor, version 2
     case WifiV2 = "3"
-    //pulse.eco LoRaWAN based sensor. version 2
+    // Pulse.eco LoRaWAN based sensor. version 2
     case LoRaWANV2 = "4"
-    case undefined = "-1"
     case Pengy = "20001"
-    //URAD Monitor device
+    // URAD Monitor device
     case URAD = "20002"
-    //AirThings platform device
+    // AirThings platform device
     case AirThings = "20003"
-    //sensor.community crowdsourced device
+    // Sensor.community crowdsourced device
     case CommunitySensor = "20004"
+    // Used as a default value
+    case undefined = "-1"
 }
 
 extension SensorType {
     public var imageForType: UIImage? {
-        #warning("Use appropriate image for Pengy")
         switch self {
         case .MOEPP: return UIImage(named: "moeppTypeIcon")
         case .LoRaWAN: return UIImage(named: "loraTypeIcon")
@@ -32,7 +36,7 @@ extension SensorType {
         case .URAD: return UIImage(named: "wifiTypeIcon")
         case .AirThings: return UIImage(named: "wifiTypeIcon")
         case .CommunitySensor: return UIImage(named: "wifiTypeIcon")
-        default: return nil
+        default: return UIImage(named: "wifiTypeIcon")
         }
     }
 }
