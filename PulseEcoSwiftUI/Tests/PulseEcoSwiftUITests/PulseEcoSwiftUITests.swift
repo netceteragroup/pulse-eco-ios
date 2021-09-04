65fb8063783aa00530ebce9cd48c7d47e6bbb199
Filip Sardjoski <Filip.Sardjoski@netcetera.com>
2021-09-04 17:30:44 +0200

    Add SPM and remove Cocoapods

---
+++ PulseEcoSwiftUI/Tests/PulseEcoSwiftUITests/PulseEcoSwiftUITests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import PulseEcoSwiftUI
+
+final class PulseEcoSwiftUITests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PulseEcoSwiftUI().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
