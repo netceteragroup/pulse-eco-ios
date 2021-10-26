6f06247690243eceb01c8bfd45123320bbf57a28
Filip Sardjoski <Filip.Sardjoski@netcetera.com>
2021-10-26 20:58:19 +0200

    [no-issue] Remove old app and move new app to root of folder

--- PulseEcoSwiftUI/README.md
+++
@@ -1,3 +0,0 @@
-# PulseEcoSwiftUI
-
-A description of this package.
--- pulse.eco/Map/Favorites/MaxSensorTooltip.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-class MaxSensorTooltip: UIView {
-	class func instanceFromNib() -> MaxSensorTooltip? {
-		return Bundle.main.loadNibNamed("MaxSensorTooltip", owner: nil, options: nil)![0] as? MaxSensorTooltip
-	}
-}
