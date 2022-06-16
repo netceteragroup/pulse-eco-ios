7da1077886a30f7282b16245ac8fa228157c3cfc
Stefan Lazarevski <Stefan.Lazarevski@netcetera.com>
2022-06-16 10:24:28 +0200

    [no-issue] Using gregorian calendar now

--- PulseEco/AppRoot/Calendar.swift
+++ PulseEco/AppRoot/Calendar.swift
@@ -2,7 +2,7 @@ import Foundation
 
 /// Global variable to use instead of Calendar.current
 public var calendar: Calendar {
-    var cal = Calendar.current
+    var cal = Calendar(identifier: .gregorian)
     cal.locale = Locale(identifier: Trema.appLanguageLocale)
     return cal
 }
