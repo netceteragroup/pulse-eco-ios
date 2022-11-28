5ed1f37d7a461a65ef2474891ae1e0862c445df3
vlokvenec <veselinka.lokvenec@netcetera.com>
2022-11-28 13:35:23 +0100

    Update CityDataWrapper.swift
    [NCA349MOBI-138] added empty values in history array

--- Shared/Models/CityDataWrapper.swift
+++ Shared/Models/CityDataWrapper.swift
@@ -54,6 +54,10 @@ class CityDataWrapper: ObservableObject {
            let today = currentValue?.values[sensorType],
            let color = measure.bands.color(for: Int(today)) {
             history.append(DayDataWrapper(date: Date.now, value: today, color: color))
+        } else {
+            if ((currentValue?.values[sensorType]?.isEmpty) == nil) {
+                history.append(DayDataWrapper(date: Date.now, value: "N/A", color: "gray"))
+            }
         }
         
         return Array(Set(history)).sorted {
