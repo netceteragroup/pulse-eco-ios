8ffcf0220d5f0ae058c5fe6213ca2129e7d11236
Nikola Jankovikj <nikolajankovik@hotmail.com>
2025-01-09 10:37:11 +0100

    [NCA349MOBI-159] removed comments

--- Shared/NetworkService.swift
+++ Shared/NetworkService.swift
@@ -194,10 +194,8 @@ class NetworkService {
             endMonth = 1
         }
         
-        let startDate = Date.from(1, selectedMonth, selectedYear)
-//        startDate = Calendar.current.date(byAdding: .day, value: -7, to: startDate!)! 
+        let startDate = Date.from(1, selectedMonth, selectedYear) 
         let endDate = Date.from(1, endMonth, endYear)
-//        endDate = Calendar.current.date(byAdding: .day, value: +1, to: endDate!)!
         let result = await downloadAverageData(for: cityName,
                                                from: startDate!,
                                                to: endDate!,
