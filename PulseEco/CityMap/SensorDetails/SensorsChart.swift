b0a4059a71ac06f30351765b63df86b304d91bb8
Nikola Jankovikj <nikolajankovik@hotmail.com>
2025-02-24 10:06:42 +0100

    [NCA349MOBI-176] replaced empty chart with text

--- PulseEco/CityMap/SensorDetails/SensorsChart.swift
+++ PulseEco/CityMap/SensorDetails/SensorsChart.swift
@@ -12,72 +12,77 @@ struct SensorsChart: View {
     @ObservedObject var viewModel: ChartViewModel
     
     var body: some View {
-        Chart {
-            ForEach(viewModel.selectedMeasure.bands, id: \.self) { band in
-                BarMark(x: .value("Time", viewModel.getMinDate()),
-                        yStart: .value("Value", band.from),
-                        yEnd: .value("Value", band.to),
-                        width: 8)
-                    .foregroundStyle(Color(band.legendColor))
-                
-                RuleMark(y: .value(band.grade, max(0, band.from - 1)))
-                    .foregroundStyle(Color(band.legendColor))
-                    .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
-            }
-            
-            if !viewModel.selectedSensorReadings.isEmpty {
-                ForEach(viewModel.selectedSensorReadings, id: \.self) { reading in
-                    LineMark(x: .value("Time",
-                                       reading.stamp),
-                             y: .value("Value",
-                                       reading.value))
+        if viewModel.selectedSensorReadings.isEmpty && viewModel.chartSensorReadings.isEmpty {
+            Text("No sensors selected")
+        }
+        else {
+            Chart {
+                ForEach(viewModel.selectedMeasure.bands, id: \.self) { band in
+                    BarMark(x: .value("Time", viewModel.getMinDate()),
+                            yStart: .value("Value", band.from),
+                            yEnd: .value("Value", band.to),
+                            width: 8)
+                        .foregroundStyle(Color(band.legendColor))
+                    
+                    RuleMark(y: .value(band.grade, max(0, band.from - 1)))
+                        .foregroundStyle(Color(band.legendColor))
+                        .lineStyle(StrokeStyle(lineWidth: 1, dash: [5]))
                 }
-            }
-            else {
-                ForEach(Array(viewModel.chartSensorReadings.enumerated()), id:\.offset) { index, sensor in
-                    ForEach(sensor) { reading in
+                
+                if !viewModel.selectedSensorReadings.isEmpty {
+                    ForEach(viewModel.selectedSensorReadings, id: \.self) { reading in
                         LineMark(x: .value("Time",
                                            reading.stamp),
                                  y: .value("Value",
                                            reading.value))
-                        .foregroundStyle(by: .value("Sensor", reading.title))
                     }
                 }
-            }
-            
-        }
-        .chartYScale(domain: 0...viewModel.maxValue())
-        .chartYAxis {
-            AxisMarks(position: .leading) {
-                AxisValueLabel()
-                    .offset(CGSize(width: -5, height: 0))
-            }
-        }
-        .chartXAxis {
-            AxisMarks(values: viewModel.getLast24H()) { value in
-                if let date = value.as(Date.self) {
-                    let hour = Calendar.current.component(.hour, from: date)
-                    AxisValueLabel {
-                        VStack(alignment: .leading) {
-                            switch hour {
-                            case 0:
-                                Text(date, format: .dateTime.month().day())
-                            default:
-                                Text(viewModel.formatTime(for: hour))
-                            }
+                else {
+                    ForEach(Array(viewModel.chartSensorReadings.enumerated()), id:\.offset) { index, sensor in
+                        ForEach(sensor) { reading in
+                            LineMark(x: .value("Time",
+                                               reading.stamp),
+                                     y: .value("Value",
+                                               reading.value))
+                            .foregroundStyle(by: .value("Sensor", reading.title))
                         }
                     }
-                    
-                    if hour == 0 {
-                        AxisTick(stroke: StrokeStyle(lineWidth: 0.5))
-                    } else {
-                        AxisTick()
+                }
+                
+            }
+            .chartYScale(domain: 0...viewModel.maxValue())
+            .chartYAxis {
+                AxisMarks(position: .leading) {
+                    AxisValueLabel()
+                        .offset(CGSize(width: -5, height: 0))
+                }
+            }
+            .chartXAxis {
+                AxisMarks(values: viewModel.getLast24H()) { value in
+                    if let date = value.as(Date.self) {
+                        let hour = Calendar.current.component(.hour, from: date)
+                        AxisValueLabel {
+                            VStack(alignment: .leading) {
+                                switch hour {
+                                case 0:
+                                    Text(date, format: .dateTime.month().day())
+                                default:
+                                    Text(viewModel.formatTime(for: hour))
+                                }
+                            }
+                        }
+                        
+                        if hour == 0 {
+                            AxisTick(stroke: StrokeStyle(lineWidth: 0.5))
+                        } else {
+                            AxisTick()
+                        }
                     }
                 }
             }
+            .padding(.horizontal, 10)
+            .padding(.top, 10)
+            .chartLegend(alignment: .center)
         }
-        .padding(.horizontal, 10)
-        .padding(.top, 10)
-        .chartLegend(alignment: .center)
     }
 }
